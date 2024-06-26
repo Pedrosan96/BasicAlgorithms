8b8289b639d9c25c4c57c1d470525ea330b45f68
Pedro Donaldo Sanchez Galvan <pedrosanchezgal711@gmail.com>
2024-06-25 22:17:59 -0600

    Repo structure first commit

---
+++ src/common/commonAlgo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+commonAlgo.py
+--------------------
+:author:        Pedrosan96
+:version:       $Revision: 0.1 $
+:contact:       $linkedin.com/in/pedro-donaldo-sanchez-gal $ (last change)
+:date:          $Date: 2023/01/18 13:00:00 GMT-6 $
+"""
+
