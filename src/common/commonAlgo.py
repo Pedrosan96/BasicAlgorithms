a4edca2c33b4c8829c0ad2ea29a1d8f01f57cb7a
Pedro Donaldo Sanchez Galvan <pedrosanchezgal711@gmail.com>
2024-10-20 15:57:20 -0600

    Added Merge sort algorithm

--- src/common/commonAlgo.py
+++ src/common/commonAlgo.py
@@ -31,25 +31,60 @@ def binary_search_sorted_iter(array: list, val, l_index: int = 0, r_index: int =
             return mid
     return -1
 
-def binary_search_sorted_recu(array: list, val, l_index: int = 0, r_index: int = None) -> int:
+def binary_search_sorted_recu(my_array: list, val, l_index: int = 0, r_index: int = None) -> int:
     """
     Iterative binary search of Value in a given sorted Array
-    :param array: Given sorted array to search in
+    :param my_array: Given sorted array to search in
     :param l_index: Left index of array
     :param r_index: Right index of array
     :param val: Value to search for
     :return: index of Value in Array, -1 if not found
     """
     if r_index is None:
-        r_index = len(array) - 1
-    if not (array[l_index] < val > array[l_index]):
+        r_index = len(my_array) - 1
+    if not (my_array[l_index] < val > my_array[l_index]):
         return -1
     mid = int((l_index + r_index) / 2)
-    if val < array[mid]:
-        binary_search_sorted_recu(array, val, l_index, mid - 1)
-    elif val > array[mid]:
-        binary_search_sorted_recu(array, val, mid + 1, r_index)
+    if val < my_array[mid]:
+        binary_search_sorted_recu(my_array, val, l_index, mid - 1)
+    elif val > my_array[mid]:
+        binary_search_sorted_recu(my_array, val, mid + 1, r_index)
     else:
         return mid
     return -1
+
+
+def merge_sort(my_array: list):
+    """
+    Merge sort algorithm
+    :param my_array: Array of numbers to sort
+    :return:
+    """
+    if len(my_array) > 1:
+        left_array = my_array[:len(my_array)//2]
+        right_array = my_array[len(my_array)//2:]
+
+        merge_sort(left_array)
+        merge_sort(right_array)
+
+        i, j, k = 0, 0, 0
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                my_array[k] = left_array[i]
+                i += 1
+            else:
+                my_array[k] = right_array[j]
+                j += 1
+
+            k += 1
+
+        while i < len(left_array):
+            my_array[k] = left_array[i]
+            i += 1
+            k += 1
+
+        while j < len(right_array):
+            my_array[k] = right_array[j]
+            j += 1
+            k += 1
 
